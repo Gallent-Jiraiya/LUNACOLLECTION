f451310cdd5e058da4087a89f41d0815541e3b7d
Gallent-Jiraiya <tharindurcg1@gmail.com>
2022-10-27 10:21:44 +0530

    add products and homepage

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,42 +1,7 @@
-import { useState } from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import './bootstrap.min.css'
+import App from './App.js'
 
-function MyForm() {
-  const [inputs, setInputs] = useState({});
-
-  const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setInputs(values => ({...values, [name]: value}))
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(inputs);
-  }
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>Enter your name:
-      <input 
-        type="text" 
-        name="username" 
-        value={inputs.username || ""} 
-        onChange={handleChange}
-      />
-      </label>
-      <label>Enter your age:
-        <input 
-          type="number" 
-          name="age" 
-          value={inputs.age || ""} 
-          onChange={handleChange}
-        />
-        </label>
-        <input type="submit" />
-    </form>
-  )
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<MyForm />);
+ReactDOM.render(<App />, document.getElementById('root'))
